f6895e009f97051dbee9cc32c8e2765db927e8c5
Dh4rma <dh4rmakk@gmail.com>
2022-02-06 01:36:00 +0900

    fix counter validation

--- pages/counter/index.js
+++ pages/counter/index.js
@@ -2,19 +2,28 @@ import { h, text } from "/modules/hyperapp.js";
 import { dispatchAction } from "/modules/hyperapp-utils.js";
 import Button from "/components/button";
 
-const state = { count: 0 };
+const state = { count: 0, flag: false };
 
 let data = { count: 0 };
 
 const actions = {
   Add: (state, count) =>
     dispatchAction(state, (s) => {
+      if (s.count == 0) {
+        s.flag = false;
+      }
       s.count = s.count + count;
+      console.log(s.count);
       return s;
     }),
   Reduce: (state, count) =>
     dispatchAction(state, (s) => {
-      s.count = s.count - count;
+      if (s.count > 0) {
+        s.count = s.count - count;
+      }
+      if (s.count == 0) {
+        s.flag = true;
+      }
       return s;
     }),
 };
@@ -25,18 +34,21 @@ const Add = (state, count) => {
 };
 
 const Reduce = (state, count) => {
-  data.count = data.count - count;
+  if (data.count > 0) {
+    data.count = data.count - count;
+  }
   return { ...state };
 };
 
 const viewCounter = (state) => {
   return h("main", {}, [
     h("h2", {}, text("data count " + data.count)),
-    Button({ name: "Add", props: { onclick: [Add, 2] } }),
-    Button({ name: "Reduce", props: { onclick: [Reduce, 2] } }),
+    Button({ name: "Add", props: { onclick: [Add, 1] } }),
+    Button({ name: "Reduce", props: { onclick: [Reduce, 1] } }),
     h("h2", {}, text("state count " + state.count)),
-    Button({ name: "Add", props: { onclick: [actions.Add, 2] } }),
-    Button({ name: "Reduce", props: { onclick: [actions.Reduce, 2] } }),
+    Button({ name: "Add", props: { onclick: [actions.Add, 1] } }),
+    Button({ name: "Reduce", props: { onclick: [actions.Reduce, 1] } }),
+    state.flag == true && h("p", {}, text("message")),
   ]);
 };
 
